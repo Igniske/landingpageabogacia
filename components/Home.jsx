cab8e29e14d9cfd585f066167ebfd8284d262ab8
Igniske <ignaciomanuel07@gmail.com>
2024-05-05 00:03:07 -0300

    fix: cambio a la imagen

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,10 +1,8 @@
-import Abogacia from '../src/assets/abogacia.jpg'
-
 function Home(){
 
     return(
         <>
-        <div style={{backgroundImage: `url(${Abogacia})`, backgroundSize: 'cover', backgroundPosition: 'center'}} className="h-screen md:h-screen">
+        <div style={{backgroundImage: "url('https://i.ibb.co/y6PS83B/abogacia.jpg')", backgroundSize: 'cover', backgroundPosition: 'center'}} className="h-screen md:h-screen">
             <div className="absolute inset-x-0 mt-32 flex justify-center items-center">
                 <div className="max-w-md md:mx-auto mt-16 mx-8 md:ml-72 rounded-lg z-20">
                     <h2 className="text-3xl mx-4 md:text-5xl text-white font-bold leading-tight">ABOGACIA, SEGURIDAD Y OTRAS</h2>
