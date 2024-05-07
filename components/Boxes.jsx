953888815b2e12f45b8eee2880122de2645bd61f
Igniske <104842998+Igniske@users.noreply.github.com>
2024-05-07 10:53:25 -0300

    style: acomodamiento de colores y bgcolor

--- components/Boxes.jsx
+++ components/Boxes.jsx
@@ -6,21 +6,19 @@ const BoxComponent = () => {
   // Define content variables
   const box1Content = (
     <div>
-      <h1 className="text-3xl text-center">Defensa de la competencia</h1>
+      <h1 className="text-xl relative top-10 md:top-20 md:text-3xl text-center">Defensa de la competencia</h1>
     </div>
   );
 
   const box2Content = (
     <div>
-      <h1>Box 2</h1>
-      <p>This is some content for Box 2</p>
+      <h1 className="text-xl relative text-white top-10 md:top-20 md:text-3xl text-center">Defensa del consumidor</h1>
     </div>
   );
 
   const box3Content = (
     <div>
-      <h1>Box 2</h1>
-      <p>This is some content for Box 2</p>
+      <h1 className="text-xl relative md:top-20 md:text-3xl text-center">Desarrollo de proyectos, financiamiento & infrastructura</h1>
     </div>
   );
 
@@ -65,37 +63,39 @@ const BoxComponent = () => {
   };
 
   return (
-    <div className="flex flex-wrap mx-4 md:mx-0 h-full relative">
-      {boxData.map((box) => (
-        <div
-          key={box.id}
-          className={`w-1/2 md:w-1/3 h-48 md:h-96 bg-${box.bgColor}-box ${
-            maximizedBox === box.id ? "maximized" : "normal"
-          } transition-all duration-500 ease-in-out`}
-          onClick={() => handleBoxClick(box.id)}
-        >
-          <div className="border border-gray-300 h-full w-full flex items-center justify-center cursor-pointer">
-            <div>{box.content}</div>
-          </div>
-        </div>
-      ))}
-      {maximizedBox && (
-        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex items-center justify-center">
-          <div
-            className={`bg-white w-full h-full p-8 maximized-content relative`}
-          >
-            <button
-              className="absolute top-4 right-4 text-3xl"
-              onClick={handleCloseBox}
+    <div className="bg-black py-16">
+        <div className="flex flex-wrap mx-4 md:mx-0 h-full relative">
+        {boxData.map((box) => (
+            <div
+            key={box.id}
+            className={`w-1/2 md:w-1/3 h-48 md:h-96 bg-${box.bgColor}-box ${
+                maximizedBox === box.id ? "maximized" : "normal"
+            } transition-all duration-500 ease-in-out`}
+            onClick={() => handleBoxClick(box.id)}
             >
-              &#10005;
-            </button>
-            <div className="text-4xl font-bold mb-4">
-              {boxData.find((box) => box.id === maximizedBox).content}
+            <div className="border border-gray-300 h-full w-full flex items-center justify-center cursor-pointer">
+                <div>{box.content}</div>
             </div>
-          </div>
+            </div>
+        ))}
+        {maximizedBox && (
+            <div className="fixed top-0 left-0 w-full h-full z-50 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+            <div
+                className={`bg-white w-full h-full p-8 maximized-content relative`}
+            >
+                <button
+                className="absolute top-4 right-4 text-3xl"
+                onClick={handleCloseBox}
+                >
+                &#10005;
+                </button>
+                <div className="text-4xl font-bold mb-4">
+                {boxData.find((box) => box.id === maximizedBox).content}
+                </div>
+            </div>
+            </div>
+        )}
         </div>
-      )}
     </div>
   );
 };
