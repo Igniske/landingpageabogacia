a5e8f11ff6adb7822181f543311db5088d7946ef
Igniske <104842998+Igniske@users.noreply.github.com>
2024-05-16 11:10:59 -0300

    feat: se agrega mailTo en form

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,6 +4,13 @@ import { useInView } from "react-intersection-observer";
 
 function Form() {
   const [isInView, setIsInView] = useState(false);
+  const [formData, setFormData] = useState({
+    nombre: "",
+    telefono: "",
+    email: "",
+    comentario: "",
+  });
+  const [isSent, setIsSent] = useState(false); // State for success message
   const { ref, inView } = useInView({
     triggerOnce: true, // Trigger animation only once
     threshold: 0.5, // Trigger animation when 50% of the element is in view
@@ -12,6 +19,62 @@ function Form() {
   useEffect(() => {
     setIsInView(inView);
   }, [inView]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    let updatedValue = value;
+
+    // Apply input validation based on the input name
+    switch (name) {
+      case "nombre":
+        updatedValue = value.replace(/[^a-zA-Z\s]/g, ""); // Allow only letters and whitespaces
+        break;
+      case "telefono":
+        updatedValue = value.replace(/[^+\d]/g, ""); // Allow only '+' and numbers
+        break;
+      case "email":
+        // Validating email format using a simple regex
+        if (value.length <= 40) updatedValue = value;
+        break;
+      case "comentario":
+        if (value.length <= 200) updatedValue = value;
+        break;
+      default:
+        break;
+    }
+
+    setFormData({ ...formData, [name]: updatedValue });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if all fields are filled
+    const allFieldsFilled = Object.values(formData).every((value) => value.trim() !== "");
+
+    if (allFieldsFilled) {
+      // Send email using mailto: link
+      const { nombre, telefono, email, comentario } = formData;
+      const mailtoLink = `mailto:youremail@example.com?subject=Form Submission&body=Nombre y Apellido: ${nombre}%0ATeléfono: ${telefono}%0AEmail: ${email}%0AComentario: ${comentario}`;
+      window.location.href = mailtoLink;
+
+      // Show success message
+      setIsSent(true);
+
+      // Clear form after 3 seconds
+      setTimeout(() => {
+        setIsSent(false);
+        setFormData({
+          nombre: "",
+          telefono: "",
+          email: "",
+          comentario: "",
+        });
+      }, 3000);
+    } else {
+      alert("Please fill in all fields.");
+    }
+  };
 
   return (
     <>
@@ -28,7 +91,7 @@ function Form() {
           </motion.p>
         </div>
         <div className="flex justify-center">
-          <form className=" w-4/5 md:w-full max-w-lg mt-8">
+          <form className=" w-4/5 md:w-full max-w-lg mt-8" onSubmit={handleSubmit}>
             <div className="mb-6">
               <label htmlFor="nombre" className="block text-lg font-medium text-white mb-2">
                 Nombre y apellido <span className="text-red-600">*</span>
@@ -36,7 +99,11 @@ function Form() {
               <input
                 type="text"
                 id="nombre"
+                name="nombre"
+                value={formData.nombre}
+                onChange={handleChange}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                required
               />
             </div>
             <div className="mb-6">
@@ -46,7 +113,11 @@ function Form() {
               <input
                 type="tel"
                 id="telefono"
+                name="telefono"
+                value={formData.telefono}
+                onChange={handleChange}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                required
               />
             </div>
             <div className="mb-6">
@@ -56,7 +127,11 @@ function Form() {
               <input
                 type="email"
                 id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                required
               />
             </div>
             <div className="mb-6">
@@ -65,7 +140,11 @@ function Form() {
               </label>
               <textarea
                 id="comentario"
+                name="comentario"
+                value={formData.comentario}
+                onChange={handleChange}
                 className="w-full px-4 py-2 border rounded-lg h-32 resize-none focus:outline-none focus:border-blue-500"
+                required
               ></textarea>
             </div>
             <div>
@@ -79,6 +158,11 @@ function Form() {
           </form>
         </div>
       </div>
+      {isSent && (
+        <div className="fixed bottom-0 left-0 right-0 bg-green-500 text-white py-2 z-50 text-center">
+          Enviado satisfactoriamente
+        </div>
+      )}
     </>
   );
 }
